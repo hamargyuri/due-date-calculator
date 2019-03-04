976cd767e72f23bcad368ba43b371e641ac5669f
György Hamar <gyurinak@gmail.com>
2019-03-04 18:21:07 +0100

    Implement due date calculation considering work hours and weekdays

--- src/main/java/DueDateCalculator.java
+++ src/main/java/DueDateCalculator.java
@@ -17,6 +17,24 @@ public class DueDateCalculator {
     private final static LocalTime WORK_DAY_START = LocalTime.of(9, 0);
     private final static LocalTime WORK_DAY_END = LocalTime.of(17, 0);
 
+    private LocalDateTime addDaysToDueDate(LocalDateTime dueDate, Integer daysToAdd) {
+        if(WORKDAYS.indexOf(dueDate.getDayOfWeek()) + daysToAdd >= WORKDAYS.size()) {
+            dueDate = dueDate.plusDays(2);
+        }
+        return dueDate.plusDays(daysToAdd);
+    }
+
+    private LocalDateTime addHoursToDueDate(LocalDateTime dueDate, Integer hoursToAdd) {
+        dueDate = dueDate.plusHours(hoursToAdd);
+        if(dueDate.toLocalTime().isAfter(WORK_DAY_END)) {
+            dueDate = dueDate.plusHours(16);
+        }
+        if(!WORKDAYS.contains(dueDate.getDayOfWeek())) {
+            dueDate = dueDate.plusDays(2);
+        }
+        return dueDate;
+    }
+
     public LocalDateTime calculateDueDate(LocalDateTime submittedAt, Integer turnaroundHours) {
         if(!WORKDAYS.contains(submittedAt.getDayOfWeek())) {
             throw new DueDateCalculatorException("Invalid day - problems must be submitted on workdays.");
@@ -24,6 +42,15 @@ public class DueDateCalculator {
         if(submittedAt.toLocalTime().isBefore(WORK_DAY_START) || submittedAt.toLocalTime().isAfter(WORK_DAY_END)) {
             throw new DueDateCalculatorException("Invalid time - problems must be submitted between 9AM - 5PM");
         }
-        return submittedAt.plusHours(turnaroundHours);
+
+        Integer totalDays = turnaroundHours / 8;
+        Integer weeksToAdd = totalDays / 5;
+        Integer daysToAdd = totalDays % 5;
+        Integer hoursToAdd = turnaroundHours % 8;
+
+        LocalDateTime dueDate = submittedAt.plusWeeks(weeksToAdd);
+        dueDate = addDaysToDueDate(dueDate, daysToAdd);
+        dueDate = addHoursToDueDate(dueDate, hoursToAdd);
+        return dueDate;
     }
 }
