745afa9b9e2f4aaceb672fe3c2475928b018973d
György Hamar <gyurinak@gmail.com>
2019-03-04 11:03:07 +0100

    Create DueDateCalculator class
    with time constraints and unimplemented calculator method

---
+++ src/main/java/DueDateCalculator.java
@@ -0,0 +1,21 @@
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
+
+public class DueDateCalculator {
+    private final static List<DayOfWeek> WORKDAYS = Arrays.asList(
+            DayOfWeek.MONDAY,
+            DayOfWeek.TUESDAY,
+            DayOfWeek.WEDNESDAY,
+            DayOfWeek.THURSDAY,
+            DayOfWeek.FRIDAY
+    );
+    private final static LocalTime WORK_DAY_START = LocalTime.of(9, 0);
+    private final static LocalTime WORK_DAY_END = LocalTime.of(17, 0);
+
+    public LocalDateTime calculateDueDate(LocalDateTime submittedAt, Integer turnaroundTime) {
+        return null;
+    }
+}
